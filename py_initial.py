fddcf29bb1703fba6a3686dfa69e93e460f60815
izunori <izuharakun53@yahoo.co.jp>
2021-09-30 22:13:19 +0900

    update py_initial.py

--- py_initial.py
+++ py_initial.py
@@ -1,4 +1,8 @@
 import sys
+from itertools import combinations, permutations, product, accumulate
+from collections import defaultdict, deque, Counter
+import heapq as hq
+import bisect
 sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 # = input().strip()
