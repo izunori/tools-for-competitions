172878f0456207c79ac90f942354baf19ed01398
izunori <izuharakun53@yahoo.co.jp>
2022-04-23 14:45:55 +0900

    Add nim_initial.nim

---
+++ nim_initial.nim
@@ -0,0 +1,7 @@
+import algorithm, deques, sequtils, strutils, sugar, tables
+
+#let S = stdin.readLine
+#let N = stdin.readLine.parseInt
+#let A = stdin.readLine.split.map parseInt
+#let C = (0..<N).mapIt(stdin.readLine.split.map parseInt)
+
