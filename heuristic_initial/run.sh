f4744dbc86d6ffec5584e7ec3235b651ad0ab03f
izunori <izuharakun53@yahoo.co.jp>
2024-07-06 15:20:37 +0900

    Update run.sh

--- heuristic_initial/run.sh
+++ heuristic_initial/run.sh
@@ -8,6 +8,7 @@ fi
 #pypy3 ./a.py < ${INPUT} > result.txt
 ./out < ${INPUT} 2>&1 |tee  result.txt
 
-cat ./result.txt | /mnt/c/WINDOWS/system32/clip.exe
+#cat ./result.txt | /mnt/c/WINDOWS/system32/clip.exe
+grep -v '#' ./result.txt | /mnt/c/WINDOWS/system32/clip.exe
 #grep -v '#' ./result.txt | iconv -f ISO2022JP -t UTF-16le | /mnt/c/WINDOWS/system32/clip.exe
 
