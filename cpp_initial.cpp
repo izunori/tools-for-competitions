2e388d9358baa6193d61cbb9bea753a04dda65bc
izunori <izuharakun53@yahoo.co.jp>
2024-11-02 12:32:05 +0900

    Update cpp_init

--- cpp_initial.cpp
+++ cpp_initial.cpp
@@ -1,35 +1,105 @@
+#pragma GCC optimize("O3,inline")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC target("movbe")
+#pragma GCC target("aes,pclmul,rdrnd")
 #include<iostream>
 #include<vector>
-#include<cmath>
-#include<cstdint>
+#include<string>
+#include<tuple>
+#include<chrono>
+#include<map>
+#include<set>
 #include<algorithm>
-#include<boost/range/irange.hpp>
-#define PRINT(text) (std::cout << (text) << std::endl);
-#define VPRINT(vec) for(const auto& x:vec){std::cout << x << ' ';};
+#include<queue>
+#include<random>
+#include<numeric>
+#include<functional>
+#include<stack>
+#include<cassert>
+#include<cmath>
+#include<cstring>
+#include<unordered_set>
+#include<unordered_map>
+#include<ranges>
+#include<bitset>
+#include<bit>
+
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define reps(i, s, n) for (int i = s; i < (int)n; i++)
+#define rep2(i,j,n) for (int i = 0; i < (int)n; i++) for (int j = 0; j < (int)n; j++)
+#define rep3(i,j,k,n) for (int i = 0; i < (int)n; i++) for (int j = 0; j < (int)n; j++) for (int k= 0; k < (int)n; k++)
+#define all(v) v.begin(),v.end()
+
 template<typename T>
-void for_each(std::vector<T> &vec, std::function<T(T)> &f){
-    std::for_each(vec.cbegin(), vec.cend(), f);
+using vec = std::vector<T>;
+template<typename T>
+using vec2 = vec<vec<T>>;
+using ll = long long;
+template<typename T>
+using t2 = std::tuple<T, T>;
+using i2 = std::pair<int,int>;
+template<typename T>
+using t3 = std::tuple<T, T, T>;
+using i3 = t3<int>;
+using clk = std::chrono::system_clock;
+
+// global
+
+std::random_device rnd;
+std::mt19937 mt(rnd());
+std::uniform_real_distribution<> rand01(0.0, 1.0);
+
+// utils
+
+template<typename T> void print(const T& v){ std::cout << v; }
+template<typename... T> void print(const std::tuple<T...>& tp);
+template<typename T>
+void print(const std::vector<T>& vs){
+    std::cout << "[";
+    rep(i, vs.size()){
+        if(i > 0) print(" ");
+        print(vs[i]);
+    }
+    std::cout << "]";
 }
+template<typename TupType, size_t... I>
+void print(const TupType& tp, std::index_sequence<I...>){
+    std::cout << "(";
+    (..., (print(I==0? "" : " "), print(std::get<I>(tp))));
+    std::cout << ")";
+}
+template<typename... T>
+void print(const std::tuple<T...>& tp){
+    print(tp, std::make_index_sequence<sizeof...(T)>());
+}
+template<class T, class... A> void print(const T& first, const A&... rest) { print(first); print(" "); print(rest...); }
+template<class... T>
+void dprint(const T&... rest){
+    std::cout << "# ";
+    print(rest...);
+    std::cout << "\n";
+}
+
 template<typename T>
-T pow(T p, T n, T m){
-    T res = 1;
-    while(n){
-        if(n&1) res = (res*p)%m;
-        p = (p*p)%m;
-        n >>= 1;
+vec<T> LI(int n){
+    vec<T> res;
+    rep(i,n){
+        T x; std::cin >> x; res.push_back(x);
     }
     return res;
 }
+
 template<typename T>
-inline auto range(T first, T last=-1, T step = 1){
-    if(last == -1){
-        return boost::irange(0, first, step);
-    } else {
-        return boost::irange(first, last, step);
-    }
+vec<T> LI2(int n, int m){
+    vec2<T> res;
+    rep(i,n) res.emplace_back(LI<T>(m));
+    return res;
 }
 
+// main
+
 int main(){
-    std::cin >> N >> X >> Y >> Z;
     return 0;
 }
